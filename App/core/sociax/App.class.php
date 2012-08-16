cda9de9f91964404fcaf4bb122c39726429b1679
DavidAQ <153019747@qq.com>
2012-08-16 15:21:23 +0800

    set language thru  GET

--- App/core/sociax/App.class.php
+++ App/core/sociax/App.class.php
@@ -339,7 +339,11 @@ class App
         }
         // 启用了语言包功能
         // 根据是否启用自动侦测设置获取语言选择
-        if (C('LANG_AUTO_DETECT')){
+        if(isset($_GET['language'])){
+        		$langSet=$_GET['language'];
+        }elseif(isset($_LANGSET)){
+        		$langSet=$_LANGSET;
+        }elseif (C('LANG_AUTO_DETECT')){
             if(isset($_GET[C('VAR_LANGUAGE')])){// 检测浏览器支持语言
                 $langSet = $_GET[C('VAR_LANGUAGE')];// url中设置了语言变量
                 cookie('think_language',$langSet,3600);
