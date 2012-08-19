3310e8c269d9fe4e35090ee32fde7f5f131f649d
DavidAQ <153019747@qq.com>
2012-08-19 22:09:56 +0800

    jspre

--- App/jspre.php
+++ App/jspre.php
@@ -1,4 +1,6 @@
 <?php
+	session_start();
+	$lanSet=isset($_SESSION['language'])&&$_SESSION['language']='en'?'en':'zh_cn';
 	$f=$_SERVER['REQUEST_URI'];	
 	$f=strstr($f,'jspre.php?');
 	$f=substr($f,strlen('jspre.php?'));
@@ -9,14 +11,10 @@
 		$file=implode('',$file);
 		if(preg_match('/\/\/.?@language=(.+)/i',$file,$match)){
 			$p=explode('/',strtolower(trim($match[1])));
-			if(file_exists('./apps/'.$p[0].'/Lang/en/common.php'))
-				$arr=include('./apps/'.$p[0].'/Lang/en/common.php');
-			if(file_exists('./apps/'.$p[0].'/Lang/zh_cn/common.php'))
-				array_push($arr,include('./apps/'.$p[0].'/Lang/zh_cn/common.php'));
-			if(file_exists('./apps/'.$p[0].'/Lang/en/'.$p[1].'.php'))
-				array_push($arr,include('./apps/'.$p[0].'/Lang/en/'.$p[1].'.php'));
-			if(file_exists('./apps/'.$p[0].'/Lang/zh_cn/'.$p[1].'.php'))
-				array_push($arr,include('./apps/'.$p[0].'/Lang/zh_cn/'.$p[1].'.php'));
+			if(file_exists('./apps/'.$p[0].'/Lang/'.$lanSet.'/common.php'))
+				$arr=include('./apps/'.$p[0].'/Lang/'.$lanSet.'/common.php');
+			if(file_exists('./apps/'.$p[0].'/Lang/'.$lanSet.'/'.$p[1].'.php'))
+				array_push($arr,include('./apps/'.$p[0].'/Lang/'.$lanSet.'/'.$p[1].'.php'));
 			function call_bbb($arg){
 				global $arr;
 				if(isset($arr[$arg[1]])){
