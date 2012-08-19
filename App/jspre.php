963dae705f6dfb3e208b96a0f21f10277ab39878
DavidAQ <153019747@qq.com>
2012-08-19 22:23:59 +0800

    jspre

--- App/jspre.php
+++ App/jspre.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	$lanSet=isset($_SESSION['language'])&&$_SESSION['language']='en'?'en':'zh_cn';
+	$lanSet=(isset($_SESSION['language'])&&$_SESSION['language']='en')?'en':'zh_cn';
 	$f=$_SERVER['REQUEST_URI'];	
 	$f=strstr($f,'jspre.php?');
 	$f=substr($f,strlen('jspre.php?'));
@@ -11,13 +11,17 @@
 		$file=implode('',$file);
 		if(preg_match('/\/\/.?@language=(.+)/i',$file,$match)){
 			$p=explode('/',strtolower(trim($match[1])));
+			$arr2=$arr=array();
 			if(file_exists('./apps/'.$p[0].'/Lang/'.$lanSet.'/common.php'))
 				$arr=include('./apps/'.$p[0].'/Lang/'.$lanSet.'/common.php');
 			if(file_exists('./apps/'.$p[0].'/Lang/'.$lanSet.'/'.$p[1].'.php'))
-				array_push($arr,include('./apps/'.$p[0].'/Lang/'.$lanSet.'/'.$p[1].'.php'));
+				$arr2=include('./apps/'.$p[0].'/Lang/'.$lanSet.'/'.$p[1].'.php');
 			function call_bbb($arg){
 				global $arr;
-				if(isset($arr[$arg[1]])){
+				global $arr2;
+				if(isset($arr2[$arg[1]])){
+					return $arr2[$arg[1]];
+				}elseif(isset($arr[$arg[1]])){
 					return $arr[$arg[1]];
 				}else{
 					return $arg[1];
