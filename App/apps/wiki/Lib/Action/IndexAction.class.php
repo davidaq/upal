d927d62c0b72f1d2f775d8ec0e329de32e99fd69
DavidAQ <153019747@qq.com>
2012-09-16 20:42:41 +0800

    a

--- App/apps/wiki/Lib/Action/IndexAction.class.php
+++ App/apps/wiki/Lib/Action/IndexAction.class.php
@@ -31,24 +31,30 @@ class IndexAction extends Action{
 	}
 	// Display search result
 	function search(){
-		if (isset($_POST['wiki_key']))
+		$res=array();
+		if (isset($_GET['wiki_key']))
 		{
-			$key = $_POST['wiki_key'];
+			$key = $_GET['wiki_key'];
 			$ret_wiki_acc = $this->wiki->searchWikiByTitleAccurate($key);
 			$ret_wiki_sim = $this->wiki->searchWikiByTitleSimilar($key);
 			$ret_tag = $this->wikiTag->searchWikiByTag($key);
+			if($ret_wiki_sim)
+				$res = array_merge($res,$ret_wiki_sim);
+			if($ret_tag)
+				$res = array_merge($res,$ret_tag);
 			$this->assign('searchresultacc',$ret_wiki_acc);
 		}
-		if (isset($_POST['wiki_tag']))
+		if (isset($_GET['wiki_tag']))
 		{
-			$key = $_POST['wiki_tag'];
-			$tag = $_POST['wiki_tag'];
+			$key = $_GET['wiki_tag'];
+			$tag = $_GET['wiki_tag'];
 			$ret_tag = $this->wikiTag->searchWikiByTag($tag);
+			if($ret_tag)
+				$res = array_merge($res,$ret_tag);
 		}
 		
-
 		$this->assign('searchkey',htmlspecialchars($key));
-		$this->assign('searchresult',array_merge($ret_wiki_sim,$ret_tag));
+		$this->assign('searchresult',$res);
 		$this->display();
 	}
 	// Create/modify a wiki word
@@ -56,8 +62,16 @@ class IndexAction extends Action{
 		if(isset($_GET['id'])){
 		}else{
 			$this->assign('keyword',$_GET['keyword']);
+			$this->assign('tags','');
 			$this->assign('id',0);			
 		}
 		$this->display();
+	}
+	public function show(){
+		if(isset($_GET['id'])){
+			$wid=intval($_GET['id']);
+			$this->wiki->where(array('id'=>$wid))->
+			$this->display();
+		}
 	}
 }
